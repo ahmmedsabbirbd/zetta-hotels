2dff956c7148105f89e9750a89f32c97516ad9ef
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-02-14 13:07:16 +0600

    Post Dynamic

--- index.php
+++ index.php
@@ -246,101 +246,25 @@
                 </div>
 
                 <div class="row lr-8 minus">
+                    <?php while( have_posts() ) : the_post(); ?>
+
                     <div class="col-lg-4 col-sm-6">
                         <a href="#" class="blog-item">
                             <div class="media">
-                                <img src="<?= get_theme_file_uri(); ?>/images/blog-item-1.jpg" class="img-fluid" alt="">
+                                <img src="<?php the_post_thumbnail(); ?>" class="img-fluid" alt="">
                             </div>
 
                             <div class="text text-center">
                                 <div class="entry-title dark-description">
                                     <span class="sub-title">Food and life</span>
-                                    <h4 class="title exb">Succed in hotel businessss</h4>
-                                    <p>Fresh food directly from our restaurant ready coocked for you</p>
+                                    <h4 class="title exb"><?php the_title(); ?></h4> 
+                                    <?php the_content(); ?>
                                 </div>
                             </div>
                         </a>
-                    </div>
+                    </div> 
 
-                    <div class="col-lg-4 col-sm-6">
-                        <a href="#" class="blog-item">
-                            <div class="media">
-                                <img src="<?= get_theme_file_uri(); ?>/images/blog-item-2.jpg" class="img-fluid" alt="">
-                            </div>
-
-                            <div class="text text-center">
-                                <div class="entry-title dark-description">
-                                    <span class="sub-title">COoking</span>
-                                    <h4 class="title exb">Cooking delicious food</h4>
-                                    <p>Fresh food directly from our restaurant ready coocked for you</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-6">
-                        <a href="#" class="blog-item">
-                            <div class="media">
-                                <img src="<?= get_theme_file_uri(); ?>/images/blog-item-3.jpg" class="img-fluid" alt="">
-                            </div>
-
-                            <div class="text text-center">
-                                <div class="entry-title dark-description">
-                                    <span class="sub-title">Fruits</span>
-                                    <h4 class="title exb">Getting fresh fruits</h4>
-                                    <p>Fresh food directly from our restaurant ready coocked for you</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-6">
-                        <a href="#" class="blog-item">
-                            <div class="media">
-                                <img src="<?= get_theme_file_uri(); ?>/images/blog-item-3.jpg" class="img-fluid" alt="">
-                            </div>
-
-                            <div class="text text-center">
-                                <div class="entry-title dark-description">
-                                    <span class="sub-title">Fruits</span>
-                                    <h4 class="title exb">Getting fresh fruits</h4>
-                                    <p>Fresh food directly from our restaurant ready coocked for you</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-6">
-                        <a href="#" class="blog-item">
-                            <div class="media">
-                                <img src="<?= get_theme_file_uri(); ?>/images/blog-item-2.jpg" class="img-fluid" alt="">
-                            </div>
-
-                            <div class="text text-center">
-                                <div class="entry-title dark-description">
-                                    <span class="sub-title">COoking</span>
-                                    <h4 class="title exb">Cooking delicious food</h4>
-                                    <p>Fresh food directly from our restaurant ready coocked for you</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-6">
-                        <a href="#" class="blog-item">
-                            <div class="media">
-                                <img src="<?= get_theme_file_uri(); ?>/images/blog-item-1.jpg" class="img-fluid" alt="">
-                            </div>
-
-                            <div class="text text-center">
-                                <div class="entry-title dark-description">
-                                    <span class="sub-title">Food and life</span>
-                                    <h4 class="title exb">Succed in hotel businessss</h4>
-                                    <p>Fresh food directly from our restaurant ready coocked for you</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+                    <?php endwhile; ?>
                 </div>
             </div>
         </section><!-- /newest-blog -->
